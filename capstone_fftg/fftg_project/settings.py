87f91a945ccbbda3789b84758df2bdc820311fca
austen cote <coteausten@gmail.com>
2020-03-04 20:25:38 -0800

    made change to debug to false

--- capstone_fftg/fftg_project/settings.py
+++ capstone_fftg/fftg_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^cg#zh(_pqt#8evg0lsbcb-vhuc*z+5q%r2$j7rh4_t)tvw-x1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['157.245.168.148']
 
